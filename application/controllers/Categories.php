7767127f703c7385666ec066210d3e62afa0eb29
Jirayu Kanda <me@acodify.com>
2018-04-16 10:47:14 +0700

    fix name controller

--- application/controllers/Categories.php
+++ application/controllers/Categories.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Categories extends My_controller {
+class Categories extends MY_Controller {
 
 	public function __construct()
 	{
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class My_controller extends CI_Controller {
+class MY_Controller extends CI_Controller {
 
 	public function __construct()
 	{
