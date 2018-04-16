3a2c065041a286619c10c9177449bd4efd8d34c9
Matha Lokanphai <mathal@tencent.co.th>
2018-04-16 11:47:06 +0700

    test update

--- application/views/content.php
+++ application/views/content.php
@@ -4,7 +4,7 @@
                 <div class="inner">
                     <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">ค้นข้อมูล</h1>
+                    <h1 class="page-header">ค้นข้อมูล123</h1>
                 </div>
             </div>
             <div class="row">
