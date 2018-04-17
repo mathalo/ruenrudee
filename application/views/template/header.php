ea4fe478a5a0134976724f28a4fdbe1b54845328
Matha Lokanphai <mathal@tencent.co.th>
2018-04-17 13:50:11 +0700

    css update

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -4,7 +4,7 @@
         <!-- HEADER SECTION -->
         <div id="top">
 
-            <nav class="navbar navbar-inverse navbar-fixed-top " style="padding-top: 10px;background-color:HotPink;"><!-- dodgerblue -->
+            <nav class="navbar navbar-inverse navbar-fixed-top " style="padding-top: 10px;background-color:#FFF0F5;"><!-- dodgerblue -->
                 <a data-original-title="Show/Hide Menu" data-placement="bottom" data-tooltip="tooltip" class="accordion-toggle btn btn-primary btn-sm visible-xs" data-toggle="collapse" href="#menu" id="menu-toggle">
                     <i class="icon-align-justify"></i>
                 </a>
