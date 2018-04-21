ae7391fd75d5c38b9ad126ff72ed25bb455029aa
MathaL <>
2018-04-21 19:44:01 +0700

    test

--- application/views/card.php
+++ application/views/card.php
@@ -54,7 +54,7 @@ th, td {
     }
 </style>
         <!--PAGE CONTENT -->
-        <div id="content">
+        <!-- <div id="content"> -->
         
              <div class="inner">  
                 <!-- <div class="row">
@@ -220,7 +220,7 @@ th, td {
 
                  
                 </div> 
-            </div>
+            <!-- </div> -->
        <!--END PAGE CONTENT -->
 
 
