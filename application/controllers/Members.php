9a550a45429841626fa4d24af00e0ef44df7e3be
Matha Lokanphai <mathal@tencent.co.th>
2018-04-23 13:38:21 +0700

    update security check

--- application/controllers/Members.php
+++ application/controllers/Members.php
@@ -145,7 +145,7 @@ class Members extends My_controller {
 
 	public function edit($id)
 	{
-		if(!isset($id) and filter_var($id, FILTER_VALIDATE_INT) === false){
+		if(!isset($id) or filter_var($id, FILTER_VALIDATE_INT) === false){
 			echo "Your variable is not an integer";
 			exit;
 		}
