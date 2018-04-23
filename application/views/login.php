d7ee6b2c4c930f68d30c80ab72897347dd91d4f2
Matha Lokanphai <mathal@tencent.co.th>
2018-04-23 14:44:13 +0700

    update captcha

--- application/views/login.php
+++ application/views/login.php
@@ -29,9 +29,25 @@
     <![endif]-->
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <script type="text/javascript">
+      var verifyCallback = function(response) {
+        alert(response);
+      };
+      var widgetId1;
+      var widgetId2;
       var onloadCallback = function() {
-        grecaptcha.render('html_element', {
+        // Renders the HTML element with id 'example1' as a reCAPTCHA widget.
+        // The id of the reCAPTCHA widget is assigned to 'widgetId1'.
+        widgetId1 = grecaptcha.render('example1', {
+          'sitekey' : 'your_site_key',
+          'theme' : 'light'
+        });
+        widgetId2 = grecaptcha.render(document.getElementById('example2'), {
           'sitekey' : 'your_site_key'
+        });
+        grecaptcha.render('login', {
+          'sitekey' : 'your_site_key',
+          'callback' : verifyCallback,
+          'theme' : 'dark'
         });
       };
     </script>
