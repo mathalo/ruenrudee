891055c591af4b082615257232e309e929bff901
Matha Lokanphai <mathal@tencent.co.th>
2018-04-23 14:45:46 +0700

    edit captcha

--- application/views/login.php
+++ application/views/login.php
@@ -37,18 +37,11 @@
       var onloadCallback = function() {
         // Renders the HTML element with id 'example1' as a reCAPTCHA widget.
         // The id of the reCAPTCHA widget is assigned to 'widgetId1'.
-        widgetId1 = grecaptcha.render('example1', {
+        widgetId1 = grecaptcha.render('login', {
           'sitekey' : 'your_site_key',
           'theme' : 'light'
         });
-        widgetId2 = grecaptcha.render(document.getElementById('example2'), {
-          'sitekey' : 'your_site_key'
-        });
-        grecaptcha.render('login', {
-          'sitekey' : 'your_site_key',
-          'callback' : verifyCallback,
-          'theme' : 'dark'
-        });
+        
       };
     </script>
 </head>
