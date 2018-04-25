6bef915c0cc6617551b74cbb673cb57f28e0c309
Matha Lokanphai <mathal@tencent.co.th>
2018-04-25 14:51:48 +0700

    remove captcha

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -103,7 +103,7 @@ $autoload['helper'] = array('url', 'form', 'file', 'cookie');
 | config files.  Otherwise, leave it blank.
 |
 */
-$autoload['config'] = array('recaptcha');
+$autoload['config'] = array();
 
 /*
 | -------------------------------------------------------------------
@@ -117,7 +117,7 @@ $autoload['config'] = array('recaptcha');
 | "codeigniter_lang.php" would be referenced as array('codeigniter');
 |
 */
-$autoload['language'] = array('recaptcha');
+$autoload['language'] = array();
 
 /*
 | -------------------------------------------------------------------
