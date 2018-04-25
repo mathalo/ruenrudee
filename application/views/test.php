3baf3fc3a049cdce5b543c93978257f1d117f937
Matha Lokanphai <mathal@tencent.co.th>
2018-04-25 15:43:34 +0700

    test captcha

--- application/views/test.php
+++ application/views/test.php
@@ -5,19 +5,9 @@
     var verifyCallback = function(response) {
       alert(response);
     };
-    var widgetId1;
-    var widgetId2;
     var onloadCallback = function() {
-      // Renders the HTML element with id 'example1' as a reCAPTCHA widget.
-      // The id of the reCAPTCHA widget is assigned to 'widgetId1'.
-      widgetId1 = grecaptcha.render('example1', {
-        'sitekey' : '6LfrblUUAAAAALiX2rvaPoijbiE_2IQVhD-qIriW',
-        'theme' : 'light'
-      });
-      widgetId2 = grecaptcha.render(document.getElementById('example2'), {
-        'sitekey' : '6LfrblUUAAAAALiX2rvaPoijbiE_2IQVhD-qIriW'
-      });
-      grecaptcha.render('example3', {
+      
+      grecaptcha.render('recaptcha', {
         'sitekey' : '6LfrblUUAAAAALiX2rvaPoijbiE_2IQVhD-qIriW',
         'callback' : verifyCallback,
         'theme' : 'dark'
@@ -26,23 +16,10 @@
   </script>
 </head>
 <body>
-  <!-- The g-recaptcha-response string displays in an alert message upon submit. -->
-  <form action="javascript:alert(grecaptcha.getResponse(widgetId1));">
-    <div id="example1"></div>
-    <br>
-    <input type="submit" value="getResponse">
-  </form>
-  <br>
-  <!-- Resets reCAPTCHA widgetId2 upon submit. -->
-  <form action="javascript:grecaptcha.reset(widgetId2);">
-    <div id="example2"></div>
-    <br>
-    <input type="submit" value="reset">
-  </form>
-  <br>
+  
   <!-- POSTs back to the page's URL upon submit with a g-recaptcha-response POST parameter. -->
   <form action="?" method="POST">
-    <div id="example3"></div>
+    <div id="recaptcha"></div>
     <br>
     <input type="submit" value="Submit">
   </form>
