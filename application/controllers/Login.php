c771db2ee876e8c7bcb282b2e11026461c4427b6
Matha Lokanphai <mathal@tencent.co.th>
2018-04-25 11:27:58 +0700

    remove captcha

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -6,36 +6,11 @@ class Login extends My_controller {
 
 	public function __construct() {
         parent::__construct();
-        $this->load->helper('captcha');
+        
     }
     
     public function index() {
     
-        $vals = array(
-            'word'          => 'Random word',
-            'img_path'      => './captcha/',
-            'img_url'       => 'http://example.com/captcha/',
-            'font_path'     => './path/to/fonts/texb.ttf',
-            'img_width'     => '150',
-            'img_height'    => 30,
-            'expiration'    => 7200,
-            'word_length'   => 8,
-            'font_size'     => 16,
-            'img_id'        => 'Imageid',
-            'pool'          => '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    
-            // White background and border, black text and red grid
-            'colors'        => array(
-                    'background' => array(255, 255, 255),
-                    'border' => array(255, 255, 255),
-                    'text' => array(0, 0, 0),
-                    'grid' => array(255, 40, 40)
-            )
-        );
-        
-        $cap = create_captcha($vals);
-        echo $cap['image'];
-        exit;
         // $this->form_validation->set_rules('username', 'Username', 'trim|required');
        $this->form_validation->set_rules('password', 'Password', 'callback_validate_user');
     
