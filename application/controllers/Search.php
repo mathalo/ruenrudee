4e08b2fa04fae60417fee77331e73e9a7df83b65
MathaL <>
2019-01-22 22:05:05 +0700

    update search where

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -85,13 +85,13 @@ class Search extends My_controller {
 			}
 
 
-			$select_query = "Select * from artifact inner join category on category.cat_id=artifact.cat_id  inner join location on location.location_id=artifact.location_id ".$where." and artifact.status!='deleted' order by artifact.artifact_no asc" ;
+			$select_query = "Select * from artifact inner join category on category.cat_id=artifact.cat_id  inner join location on location.location_id=artifact.location_id ".$where." and artifact.status='open' order by artifact.artifact_no asc" ;
 			$query = $this->db->query($select_query);
 			$data['where'] = $select_query;
 			
 		}else{
 			// $query = $this->db->get('artifact');
-			$select_query = "Select * from artifact inner join category on category.cat_id=artifact.cat_id  inner join location on location.location_id=artifact.location_id where artifact.status!='deleted' order by artifact.artifact_no asc";
+			$select_query = "Select * from artifact inner join category on category.cat_id=artifact.cat_id  inner join location on location.location_id=artifact.location_id where artifact.status='open' order by artifact.artifact_no asc";
 			
 			$query = $this->db->query($select_query);
 			$data['where'] = $select_query;
