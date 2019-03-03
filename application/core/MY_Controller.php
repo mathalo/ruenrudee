ad397c39ab37e20b660236a3b3a5f7c39568da0a
MathaL <>
2019-03-03 16:16:20 +0700

    no message

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -32,17 +32,16 @@ class MY_Controller extends CI_Controller {
 			if($segment1 == 'members'){
 				redirect('search'); 
 			}
-		}else if($this->session->logged_in['permission']=='content' ){
-			if($segment1 != 'search' || $segment1 != 'artifact'){
-				redirect('search'); 
-			}
-		}else if($this->session->logged_in['permission']=='guess' ){
-			if($segment1 != 'search'){
-				redirect('search'); 
-			}
-		}else if ($this->session->logged_in['permission']!='' ){
-			redirect('search'); 
 		}
+		// else if($this->session->logged_in['permission']=='content' ){
+		// 	if($segment1 != 'search' || $segment1 != 'artifact'){
+		// 		redirect('search'); 
+		// 	}
+		// }else if($this->session->logged_in['permission']=='guess' ){
+		// 	if($segment1 != 'search'){
+		// 		redirect('search'); 
+		// 	}
+		// }
 		return true;
 	}
 
