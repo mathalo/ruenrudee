cf29eeb7dfbf76d8cd5231f647bc94506b4b38ff
MathaL <>
2019-03-11 20:54:26 +0700

    no message

--- application/views/artifactEdit.php
+++ application/views/artifactEdit.php
@@ -390,11 +390,11 @@
                             การแก้ไขข้อมูล
                         </div>
                         
-                        <div class="panel-body">
+                        <div >
                             
                             <div class="table-responsive">
                                 
-                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                <table class="table table-bordered table-hover" id="dataTables-example">
                                     <thead>
                                         <tr>
                                             <th>No.</th>
